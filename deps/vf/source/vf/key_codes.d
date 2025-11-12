00d4d738a06cfc73bc67647e07fc25c34b9bdc80
Vital Fadeev <vital.fadeev@gmail.com>
2025-11-13 05:46:25 +0700

    upd

--- deps/vf/source/vf/key_codes.d
+++ deps/vf/source/vf/key_codes.d
@@ -33,12 +33,18 @@ enum ulong EVT_KEY_LEFTCTRL_RELEASED = (EVT_KEY_RELEASED  << 32) | EVT_KEY_LEFTC
 //
 enum ulong EVT_REL_MOVED             = (0) | EVT_REL;
 
-enum       EVT_UI                    = 0x0200;
-enum ulong UI_POINTER_IN             = (2     << 16) | EVT_UI;;
-enum ulong UI_POINTER_OVER           = (3     << 16) | EVT_UI;;
-enum ulong UI_POINTER_OUT            = (4     << 16) | EVT_UI;;
-
 enum       EVT_APP                   = 0x0100;
 enum       APP_CODE_QUIT             = 0x0001;
 enum ulong EVT_APP_QUIT              = (APP_CODE_QUIT     << 16) | EVT_APP;
+
+enum       EVT_UI                    = 0x0200;
+enum ulong UI_POINTER_IN             = (2     << 16) | EVT_UI;
+enum ulong UI_POINTER_OVER           = (3     << 16) | EVT_UI;
+enum ulong UI_POINTER_OUT            = (4     << 16) | EVT_UI;
+
+enum       EVT_DEV                   = 0x0300;
+enum       DEV_CODE_NEW_DEVICE       = 0x0001;
+enum       DEV_CODE_REMOVE_DEVICE    = 0x0002;
+enum ulong EVT_DEV_NEW_DEVICE        = (DEV_CODE_NEW_DEVICE     << 16) | EVT_DEV;
+enum ulong EVT_DEV_REMOVE_DEVICE     = (DEV_CODE_REMOVE_DEVICE  << 16) | EVT_DEV;
 
